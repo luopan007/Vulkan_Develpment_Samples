0bb32c330ba94311dc3e644b6d9cd8aebdd79dae
luopan <709165253@qq.com>
2020-09-20 10:19:45 +0800

    平移变换

---
+++ Sample4_4/app/src/main/cpp/bndev/CubeData.h
@@ -0,0 +1,12 @@
+#ifndef VULKANEXBASE_BALLDATA_H
+#define VULKANEXBASE_BALLDATA_H
+
+class CubeData
+{
+public:
+    static float* vdata;
+    static int dataByteCount;
+    static int vCount;
+    static void  genBallData();
+};
+#endif
