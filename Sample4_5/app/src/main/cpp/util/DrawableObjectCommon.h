0a11a4c1972c45a32c8aa21d3e916ff87801576e
luopan <709165253@qq.com>
2020-09-20 10:20:55 +0800

    旋转变换

---
+++ Sample4_5/app/src/main/cpp/util/DrawableObjectCommon.h
@@ -0,0 +1,24 @@
+#ifndef VULKANEXBASE_OBJOBJECT_H
+#define VULKANEXBASE_OBJOBJECT_H
+
+#include <vulkan/vulkan.h>
+#include "vulkan_wrapper.h"
+#include <string>
+
+class DrawableObjectCommonLight
+{
+public:
+    float* pushConstantData;							//需推送的常量数据数组的首地址指针
+    VkDevice* devicePointer;
+    float* vdata;
+    int vCount;
+    VkBuffer vertexDatabuf;
+    VkDeviceMemory vertexDataMem;
+    VkDescriptorBufferInfo vertexDataBufferInfo;
+    DrawableObjectCommonLight(float* vdataIn,int dataByteCount,int vCountIn,VkDevice& device,VkPhysicalDeviceMemoryProperties& memoryroperties);
+    ~DrawableObjectCommonLight();
+    void drawSelf(VkCommandBuffer& secondary_cmd, VkPipelineLayout& pipelineLayout,VkPipeline& pipeline,VkDescriptorSet* desSetPointer);
+};
+
+
+#endif
