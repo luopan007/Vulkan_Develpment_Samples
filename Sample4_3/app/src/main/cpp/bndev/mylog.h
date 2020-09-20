4a1cdb3af7ed2d82eea213f4d2a01345e95faafa
luopan <709165253@qq.com>
2020-09-20 10:17:41 +0800

    透视投影

---
+++ Sample4_3/app/src/main/cpp/bndev/mylog.h
@@ -0,0 +1,12 @@
+
+
+#ifndef MY_LOG_H
+#define MY_LOG_H
+
+#include <android/log.h>
+
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "native-activity", __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "native-activity", __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, "native-activity", __VA_ARGS__))
+
+#endif
---
+++ Sample4_3/app/src/main/java/com/bn/Sample4_3/MyActivity.java
@@ -0,0 +1,7 @@
+package com.bn.Sample4_3;
+
+public class MyActivity extends android.app.NativeActivity {
+  static {
+    System.loadLibrary("bn-vulkan-lib");
+  }
+}
