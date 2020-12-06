1be40631e3b2d44aae7140f0ef17c5643a86545e
luopan <709165253@qq.com>
2020-12-06 17:31:52 +0800

    新增帮助脚本

--- help.bat
+++ help.bat
@@ -9,17 +9,34 @@ REM # 复制项目属性
 set gradle_properties=gradle.properties
 set gradle_properties_path="D:\Project\Vulkan_Develpment_Samples\Sample1_1\gradle.properties"
 
-REM # 开始遍历文件
+REM # 项目全局配置 
+set local_properties=local.properties
+set local_properties_path="D:\Project\Vulkan_Develpment_Samples\Sample1_1\local.properties"
+
+REM # 依次编译项目
+set project_root_startwith=Sample
+
+for /d %%a in (*) do (
+    cd %%a
+	call gradlew clean assembledebug
+	cd ..
+)
+
+REM # 开启延迟变量
 setlocal enabledelayedexpansion
+REM # 开始遍历文件
 for /r %workingDir% %%i in (. *) do ( 
     set "abs=%%i"
     set "fileName=%%~nxi"
-    if "!fileName!" equ "!targetFile!" (
+REM    if "!fileName!" equ "!targetFile!" (
 REM #	    xcopy !targetproperties! !abs! /s /e
-	)
-	if "!fileName!" equ "!gradle_properties!" (
-	    xcopy !gradle_properties_path! !abs! /s /e
-	)
+REM	)
+REM	if "!fileName!" equ "!gradle_properties!" (
+REM #	    xcopy !gradle_properties_path! !abs! /s /e
+REM	)
+REM	if "!fileName!" equ "!local_properties!" (
+REM # 	    xcopy !local_properties_path! !abs! /s /e
+REM	)
 )
 setlocal disabledelayedexpansion
 
